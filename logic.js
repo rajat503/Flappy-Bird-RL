8d1dde0904375c8a9c3a380f9de38e875b3b65cd
rajat503 <rajat503@gmail.com>
2016-08-27 19:52:50 +0530

    added constant values

--- logic.js
+++ logic.js
@@ -1,17 +1,27 @@
+var SVG="http://www.w3.org/2000/svg";
+var PIPE_WIDTH=70;
+var PIPE_GAP=150;
+var ARENA_HEIGHT=720;
+var ARENA_WIDTH=1080;
+var BIRD_WIDTH=40;
+var BIRD_HEIGHT=40;
+var DISTANCE_BETWEEN_PIPES=380;
+var UPPER_LIMIT=ARENA_HEIGHT*0.75;
+var LOWER_LIMIT=ARENA_HEIGHT*0.25;
+
 function Pipe(x, y) {
-    this.width = 50;
     this.x = x;
     this.y = y;
 }
 
 function Bird() {
     i = 0;
-    this.y=10;
-    this.height=50;
-    this.width=50;
+    this.y=ARENA_HEIGHT/2;
+    this.height=BIRD_HEIGHT;
+    this.width=BIRD_WIDTH;
     var goUp = function(bird) {
         setTimeout(function() {
-            bird.y+=0.1;
+            bird.y-=0.1;
             i++;
             console.log(bird.y);
             if(i<10)
@@ -24,58 +34,64 @@ function Bird() {
         goUp(this);
     }
     this.goDown = function() {
-        this.y-=0.1;
-        if(this.y<0)
-            this.y=0
+        this.y+=2;
+        if(this.y>ARENA_HEIGHT-BIRD_HEIGHT)
+            this.y=ARENA_HEIGHT-BIRD_HEIGHT;
     }
 }
 
 function World() {
     this.pipes=[];
-    this.pipes.push(new Pipe(500, 50));
-    this.pipes.push(new Pipe(700, 30));
-    this.pipes.push(new Pipe(900, 100));
+    this.pipes.push(new Pipe(ARENA_WIDTH, 400));
+    this.pipes.push(new Pipe(ARENA_WIDTH+DISTANCE_BETWEEN_PIPES, 450));
+    this.pipes.push(new Pipe(ARENA_WIDTH+2*DISTANCE_BETWEEN_PIPES, 350));
     console.log(this.pipes);
-    world_referece=this;
+    world_reference=this;
 
     pipe_interval=setInterval(function() {
-        for(i=0;i<world_referece.pipes.length;i++) {
-            if(world_referece.pipes[i].x+world_referece.pipes[i].width <= 0) {
-                world_referece.pipes[i].x = 500;
-                world_referece.pipes[i].y = parseInt(Math.random() * (500 - 190) + 190);
+        for(i=0;i<world_reference.pipes.length;i++) {
+            if(world_reference.pipes[i].x+PIPE_WIDTH <= 0) {
+                world_reference.pipes[i].x = ARENA_WIDTH;
+                world_reference.pipes[i].y = parseInt(Math.random()*(UPPER_LIMIT-LOWER_LIMIT)+LOWER_LIMIT);
             }
-            world_referece.pipes[i].x-=1;
-            console.log(world_referece.pipes[i]);
+            world_reference.pipes[i].x-=2;
+            console.log(world_reference.pipes[i]);
         }
     }, 10);
 
-    // var bird=new Bird();
-    // bird.flap();
-    // setInterval(function() {
-    //     if(bird.y>0) {
-    //         bird.goDown();
-    //         console.log(bird.y);
-    //     }
-    //     else {
-    //         clearInterval(this)
-    //         clearInterval(pipe_interval)
-    //     }
-    // }, 100);
+    this.bird=new Bird();
+    this.bird.flap();
+    setInterval(function() {
+        if(world_reference.bird.y<ARENA_HEIGHT-BIRD_HEIGHT) {
+            world_reference.bird.goDown();
+            console.log(world_reference.bird.y);
+        }
+        else {
+            clearInterval(this)
+            clearInterval(pipe_interval)
+        }
+    }, 10);
 }
-// var world=new World();
 
 $(document).ready(function() {
-    var SVG="http://www.w3.org/2000/svg";
-    var PIPE_GAP=50;
-    var ARENA_HEIGHT=720;
-    var ARENA_WIDTH=1080;
     var world=new World();
+
+    p=document.createElementNS(SVG, "rect");
+    p.setAttribute("x", 335);
+    p.setAttribute("y", world.bird.y);
+    p.setAttribute("height", world.bird.height);
+    p.setAttribute("width", world.bird.width);
+    p.setAttribute("class", "bird");
+    p.setAttribute("fill", "red");
+
+    $('.arena').append(p);
+
     for(i=0;i<world.pipes.length;i++) {
         p=document.createElementNS(SVG, "rect");
         p.setAttribute("x", world.pipes[i].x);
         p.setAttribute("y", 0);
         p.setAttribute("height", world.pipes[i].y);
-        p.setAttribute("width", world.pipes[i].width);
+        p.setAttribute("width", PIPE_WIDTH);
         p.setAttribute("class", "p"+i);
         $('.arena').append(p);
 
@@ -83,12 +99,14 @@ $(document).ready(function() {
         p.setAttribute("x", world.pipes[i].x);
         p.setAttribute("y", world.pipes[i].y+PIPE_GAP);
         p.setAttribute("height", ARENA_HEIGHT-world.pipes[i].y-PIPE_GAP);
-        p.setAttribute("width", world.pipes[i].width);
+        p.setAttribute("width", PIPE_WIDTH);
         p.setAttribute("class", "pl"+i);
         $('.arena').append(p);
     }
 
     setInterval(function(){
+        $('.bird').attr("y", world.bird.y);
+
         for(i=0;i<world.pipes.length;i++) {
             class_name=".p"+i;
             $(class_name).attr("x", world.pipes[i].x);
@@ -102,19 +120,4 @@ $(document).ready(function() {
 
         }
     }, 0);
-    // var element = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    // element.setAttribute("x", "500");
-    // element.setAttribute("y", "500");
-    // element.setAttribute("height", "50");
-    // element.setAttribute("width", "50");
-    // element.setAttribute("class", "rect1");
-    // $('.arena').append(element);
 });
-//
-// setInterval(function() {
-//     var old_x = parseInt($('.rect1').attr('x'));
-//     $('.rect1').attr('x', old_x-1);
-//     if(old_x<=0) {
-//         $('.rect1').remove();
-//     }
-// }, 5);
